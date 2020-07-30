ce8eda0e40a175386e029ea176bdd9c0135bf099
yongjunlee <chesidyong@gmail.com>
2020-07-30 19:59:33 +0900

    SQL 코드 형식을 수정하라

--- redshift_testing.py
+++ redshift_testing.py
@@ -29,7 +29,13 @@ def get_Redshift_connection():
 
 cur = get_Redshift_connection()
 
-sql = "SELECT to_char(st.ts, 'YYYY-MM') as Monthly, count(DISTINCT usc.userid) as ActiveUser FROM raw_data.session_timestamp st JOIN raw_data.user_session_channel usc ON st.sessionid = usc.sessionid GROUP BY to_char(st.ts, 'YYYY-MM') ORDER BY 1"
+sql = """SELECT to_char(st.ts, 'YYYY-MM') as
+Monthly, count(DISTINCT usc.userid) as ActiveUser
+FROM raw_data.session_timestamp st
+JOIN raw_data.user_session_channel usc ON
+st.sessionid = usc.sessionid
+GROUP BY 1
+ORDER BY 1"""
 
 cur.execute(sql)
 rows = cur.fetchall()
